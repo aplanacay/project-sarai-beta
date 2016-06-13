2aa4eb66ea4848be274540a5f38ee144e2926906
adrian <aplanacay@gmail.com>
2016-06-13 13:46:33 +0800

    Added footer

---
+++ client/modules/main/v1/components/footer.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import classNames from 'classnames';
+
+class Footer extends React.Component{
+	render(){
+		const footerClass = classNames('mdl-cell mdl-cell--12-col-desktop mdl-cell--8-col-tablet mdl-col--4-col-phone')
+		const sectionClass = classNames('mdl-cell--4-col-desktop mdl-cell--8-col-tablet mdl-col--4-col-phone')
+
+		return (
+			<div className="container">
+				<div className={footerClass}>
+					<div className="footer mdl-grid">
+						<div className={sectionClass}>
+							<div className="sectionTitle">
+								<h4>
+									About SARAI
+								</h4>
+							</div>
+							<div className="sectionContent">
+								Project SARAI aims to develop a national crop 
+								forecasting and monitoring system for first six priority 
+								crops: rice, corn, banana, coconut, coffee, cacao. 
+
+							*logo
+							</div>
+						</div>
+						<div className={sectionClass}>
+							<div className="sectionTitle">
+								<h4>
+									Stay Connected
+								</h4>
+							</div>
+							<div className="sectionContent">
+								<p>
+									<i className="glyphicon glyphicon-phone-alt"></i>
+									+63 (049) 536 3080, 2836
+								</p>
+								<p>
+									*email address
+									<a className="email" href="mailto:http://sarai@uplb.edu.ph">sarai@uplb.edu.ph</a>
+								</p>
+								<p>
+									*address
+									SESAM UPLB, College, Laguna Philippines 4031
+								</p>
+							</div>
+						</div>
+						<div className={sectionClass}>
+							<div className="sectionTitle">
+								<h4>
+									Get Involved
+								</h4>
+							</div>
+							<div className="sectionContent">
+								*fb *twirrer *yt	
+							</div>
+						</div>
+					</div>
+					<div className="footer-bottom mdl-grid">
+						
+							<div className="bottom-bottom-left mdl-cell--6-col-desktop mdl-cell--2-col-tablet" mdl-col--2-col-phone>
+								Copyright 2016 - Project SARAI
+							</div>
+							<div className="bottom-right mdl-cell--6-col-desktop mdl-cell--2-col-tablet mdl-cell-col--2-phone">
+								Developed by SARAI Project 3
+							</div>
+						
+					</div>
+				</div>
+			</div>
+
+		)
+	}
+}
+
+export default Footer;
+
+					
--- client/modules/main/v1/containers/home-page.js
+++ client/modules/main/v1/containers/home-page.js
@@ -2,13 +2,15 @@ import React from 'react';
 import {useDeps, composeAll, compose} from 'mantra-core';
 
 import Header from './main-header';
+import Footer from './main-footer';
 
 import HomePage from './../components/home-page.jsx';
 
 const composer = ({}, onData) => {
   const header = React.createElement(Header)
+  const footer = React.createElement(Footer)
 
-  onData(null, {header})
+  onData(null, {header, footer})
 }
 
 export default composeAll(
